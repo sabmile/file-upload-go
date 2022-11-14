c7bdee49e99835150e69b2f370a7358ce94078d4
sabmile <sab288@yandex.ru>
2022-11-14 15:45:02 +0600

    handle multiple files

--- main.go
+++ main.go
@@ -10,14 +10,14 @@ import (
 	"time"
 )
 
-const MAX_UPLOAD_FILE = 1024 * 1024 // 1 MB
+const MAX_UPLOAD_FILE = 10 * 1024 * 1024 // 10 MB
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-type", "text/html")
 	http.ServeFile(w, r, "index.html")
 }
 
-func uploadHandler(w http.ResponseWriter, r *http.Request) {
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
 		return
@@ -79,10 +79,80 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Upload succesful")
 }
 
+func uploadFilesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if err := r.ParseMultipartForm(32 << 20); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	files := r.MultipartForm.File["file"]
+
+	for _, fileHeader := range files {
+		if fileHeader.Size > MAX_UPLOAD_FILE {
+			http.Error(w, fmt.Sprintf("The uploaded image is too big: %s. Please use an image less than 1MB in size", fileHeader.Filename), http.StatusBadRequest)
+			return
+		}
+
+		file, err := fileHeader.Open()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		defer file.Close()
+
+		buff := make([]byte, 512)
+		_, err = file.Read(buff)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		filetype := http.DetectContentType(buff)
+		if filetype != "image/png" && filetype != "image/jpeg" {
+			http.Error(w, "The provided file format is not allowed. Please upload a JPEG or PNG image", http.StatusBadRequest)
+			return
+		}
+
+		_, err = file.Seek(0, io.SeekStart)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		err = os.MkdirAll("./uploads", os.ModePerm)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		f, err := os.Create(fmt.Sprintf("./uploads/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		defer f.Close()
+
+		_, err = io.Copy(f, file)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Upload succesfull")
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/upload", uploadHandler)
+	mux.HandleFunc("/upload", uploadFilesHandler)
 
 	if err := http.ListenAndServe(":4500", mux); err != nil {
 		log.Fatal(err)
